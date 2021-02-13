6405e9f3aa6812a2a720be96441499a814ffa698
dahln <dahln@outlook.com>
2021-02-13 12:00:37 -0700

    Reading DHT11 Sensor

--- temphumi/Program.cs
+++ temphumi/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Device.I2c;
+using System.Threading;
+using Iot.Device.Common;
+using Iot.Device.DHTxx;
 
 namespace temphumi
 {
@@ -6,7 +10,23 @@ namespace temphumi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                // GPIO Pin
+            using (Dht11 dht = new Dht11(4))
+            {
+                while(true)
+                {
+                    var temperature = dht.Temperature;
+                    var humidity = dht.Humidity;
+
+                    if (dht.IsLastReadSuccessful && temperature.DegreesCelsius >= -20 && temperature.DegreesCelsius <= 60)
+                    {
+                        Console.WriteLine($"Temperature: {Math.Round(dht.Temperature.DegreesFahrenheit, 2)} \u00B0F, Humidity: {dht.Humidity.Percent} %");
+                    }
+
+                    //This is a slow sensor. You can only ready approx. every 2 seconds.
+                    Thread.Sleep(2000);
+                }
+            }
         }
     }
 }
