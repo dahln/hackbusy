13042816787a34eff0d8efb0ddf4e1acdd8f323d
dahln <dahln@outlook.com>
2021-03-05 12:34:10 -0700

    Output temp and humidity info to an LCD screen attached to the pi.

--- temphumi/Program.cs
+++ temphumi/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Device.Gpio;
 using System.Device.I2c;
 using System.Threading;
+using Iot.Device.CharacterLcd;
+using Iot.Device.Pcx857x;
 using Iot.Device.Common;
 using Iot.Device.DHTxx;
 
@@ -10,7 +13,18 @@ namespace temphumi
     {
         static void Main(string[] args)
         {
-                // GPIO Pin
+            using I2cDevice i2c = I2cDevice.Create(new I2cConnectionSettings(1, 0x27));
+            using var driver = new Pcf8574(i2c);
+            using var lcd = new Lcd2004(registerSelectPin: 0, 
+                                    enablePin: 2, 
+                                    dataPins: new int[] { 4, 5, 6, 7 }, 
+                                    backlightPin: 3, 
+                                    backlightBrightness: 0.1f, 
+                                    readWritePin: 1, 
+                                    controller: new GpioController(PinNumberingScheme.Logical, driver));
+
+
+            // GPIO Pin
             using (Dht11 dht = new Dht11(4))
             {
                 while(true)
@@ -20,7 +34,16 @@ namespace temphumi
 
                     if (dht.IsLastReadSuccessful && temperature.DegreesCelsius >= -20 && temperature.DegreesCelsius <= 60)
                     {
-                        Console.WriteLine($"Temperature: {Math.Round(dht.Temperature.DegreesFahrenheit, 2)} \u00B0F, Humidity: {dht.Humidity.Percent} %");
+                        string temperatureString = $"{Math.Round(dht.Temperature.DegreesFahrenheit, 2)}F   {Math.Round(dht.Temperature.DegreesCelsius, 2)}C";
+                        string humidityString = $"Humidity: {dht.Humidity.Percent}%";
+
+                        lcd.Clear();
+
+                        lcd.SetCursorPosition(0,0);
+                        lcd.Write(temperatureString);
+
+                        lcd.SetCursorPosition(0,1);
+                        lcd.Write(humidityString);
                     }
 
                     //This is a slow sensor. You can only ready approx. every 2 seconds.
