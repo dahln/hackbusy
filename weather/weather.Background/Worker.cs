d9ab5cf3d817f727e0d4bd7c055c899529c724c1
dahln <dahln@outlook.com>
2021-03-11 20:27:30 -0700

    Background working is displaying current value to LCD on RPI

--- weather/weather.Background/Worker.cs
+++ weather/weather.Background/Worker.cs
@@ -3,8 +3,14 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
+using System.Linq;
+using System.Device.Gpio;
+using System.Device.I2c;
+using Iot.Device.CharacterLcd;
+using Iot.Device.Pcx857x;
+using Iot.Device.Common;
+using Iot.Device.DHTxx;
 using System.Threading.Tasks;
 using weather.Logic;
 using weather.Logic.Database;
@@ -24,16 +30,49 @@ namespace weather.Background
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            using (var scope = Services.CreateScope())
             {
-                using (var scope = Services.CreateScope())
-                {
-                    var _db = scope.ServiceProvider.GetRequiredService<WeatherDBContext>();
-                    var utilityLogic = scope.ServiceProvider.GetRequiredService<BusinessLogic>();
-                }
+                var _db = scope.ServiceProvider.GetRequiredService<WeatherDBContext>();
+                var utilityLogic = scope.ServiceProvider.GetRequiredService<BusinessLogic>();
 
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                //Setup LCD Screen
+                using I2cDevice i2c = I2cDevice.Create(new I2cConnectionSettings(1, 0x27));
+                using var driver = new Pcf8574(i2c);
+                using var lcd = new Lcd2004(registerSelectPin: 0, 
+                                        enablePin: 2, 
+                                        dataPins: new int[] { 4, 5, 6, 7 }, 
+                                        backlightPin: 3, 
+                                        backlightBrightness: 0.1f, 
+                                        readWritePin: 1, 
+                                        controller: new GpioController(PinNumberingScheme.Logical, driver));
+
+
+                // GPIO Pin
+                using (Dht11 dht = new Dht11(4))
+                {
+                    while(!stoppingToken.IsCancellationRequested)
+                    {
+                        var temperature = dht.Temperature;
+                        var humidity = dht.Humidity;
+
+                        if (dht.IsLastReadSuccessful && temperature.DegreesCelsius >= -20 && temperature.DegreesCelsius <= 60)
+                        {
+                            string temperatureString = $"{Math.Round(dht.Temperature.DegreesFahrenheit, 2)}F   {Math.Round(dht.Temperature.DegreesCelsius, 2)}C";
+                            string humidityString = $"Humidity: {dht.Humidity.Percent}%";
+
+                            lcd.Clear();
+
+                            lcd.SetCursorPosition(0,0);
+                            lcd.Write(temperatureString);
+
+                            lcd.SetCursorPosition(0,1);
+                            lcd.Write(humidityString);
+                        }
+
+                        //This is a slow sensor. You can only ready approx. every 2 seconds.
+                        await Task.Delay(2000, stoppingToken);
+                    }
+                }
             }
         }
     }
--- weather/weather.Logic/Database/WeatherDBContext.cs
+++ weather/weather.Logic/Database/WeatherDBContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Text;
