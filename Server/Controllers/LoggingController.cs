f4c46e5ce3a603d706104c626dc86efb1475ae0d
dahln <dahln@outlook.com>
2021-04-14 22:16:07 -0600

    Server search API - initial method

--- Server/Controllers/LoggingController.cs
+++ Server/Controllers/LoggingController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using daedalus.Shared;
 using daedalus.Server.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace daedalus.Server.Controllers
 {
@@ -35,6 +36,48 @@ namespace daedalus.Server.Controllers
             await _db.SaveChangesAsync();
 
             return Ok(loggedCondition.Id);
+        }
+
+        // [Route("api/v1/log/seed")]
+        // [HttpGet]
+        // async public Task<IActionResult> Seed() 
+        // {
+        //     for(int a = 0; a < 100; a++)
+        //     {
+        //         LoggedCondition loggedCondition = new LoggedCondition()
+        //         {
+        //             LoggedAt = DateTime.UtcNow,
+        //             DegreesCelsius = 25,
+        //             AltitudeCentimeters = 35,
+        //             HumidityPercentage = 45,
+        //             PressureMillibars = 55
+        //         };
+        //         _db.Conditions.Add(loggedCondition);
+        //     }
+
+        //     await _db.SaveChangesAsync();
+
+        //     return Ok("Done");
+        // }
+
+        // [Route("api/v1/log/clear")]
+        // [HttpGet]
+        // async public Task<IActionResult> Clear() 
+        // {
+        //     var allConditions = _db.Conditions.Where(c => c.Id != null);
+        //     _db.Conditions.RemoveRange(allConditions);
+        //     await _db.SaveChangesAsync();
+
+        //     return Ok("Cleared");
+        // }
+
+        [Route("api/v1/log/search")]
+        [HttpGet]
+        async public Task<IActionResult> SearchCondition() 
+        {
+            var results = await _db.Conditions.ToListAsync();
+
+            return Ok(results);
         }
     }
 }
